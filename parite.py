abb8b626add7b562775f699857972ac7ce82e418
MedIAIA <mohammed.harroud.fr@gmail.com>
2021-03-30 01:36:21 +0200

    prep 2

--- parite.py
+++ parite.py
@@ -2,8 +2,21 @@
 #os find the interpreterpath 
 # coding: utf-8
 #for accents , unnecessary for python3
+
+import os
+
+def launch_analysis(data_file):
+	pass
+
+
 def main():
 	print("Run")
+	
+	launch_analysis('current_mps.csv')
+	with open(path_to_file, 'r') as file:
+		file:
+			pass
+		
 
 
 
