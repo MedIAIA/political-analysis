ef9d6b97cfa0325637c00ec8bcd1d64b6c214e62
MedIAIA <mohammed.harroud.fr@gmail.com>
2021-03-30 23:02:36 +0200

    adding the analysis launcher  with command line

--- parite.py
+++ parite.py
@@ -2,16 +2,28 @@
 #os find the interpreterpath 
 # coding: utf-8
 #for accents , unnecessary for python3
-
+import argparse
 import analysis.csv as c_an
 import analysis.xml as x_an
 
 
-def main():
-	
-	c_an.launch_analysis('current_mps.csv')
-	x_an.launch_analysis('SyceronBrut.xml')
+def parse_arguments(): # Launch analysis with command line and the choose of file for analysis( CSV or XML in option of command )
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-e", "--extension", help= """Type of file to analyse. Is it a CSV or an XML?""") # doc terminal : python parite.py --help
+	parser.add_argument("-d", "--datafile", help= """ CSV file or containing the data for analysis""")
+	return  parser.parse_args()
 
+
+
+
+def main():
+	args = parse_arguments()
+	datafile = args.datafile
+	if args.extension == 'csv':
+		c_an.launch_analysis(datafile)
+	elif args.extension == 'xml':
+		x_an.launch_analysis(datafile)
+  
 
 if __name__ == "__main__":
 	main()
