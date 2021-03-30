1c19cc1d92e3ea94e81e7f3ec6838da8e9b7c7ef
MedIAIA <mohammed.harroud.fr@gmail.com>
2021-03-31 01:49:48 +0200

    adding a structure to control the exceptions at level the parite.py file

--- parite.py
+++ parite.py
@@ -5,6 +5,7 @@
 import argparse
 import analysis.csv as c_an
 import analysis.xml as x_an
+import logging as lg
 
 
 def parse_arguments(): # Launch analysis with command line and the choose of file for analysis( CSV or XML in option of command )
@@ -18,11 +19,29 @@ def parse_arguments(): # Launch analysis with command line and the choose of fil
 
 def main():
 	args = parse_arguments()
-	datafile = args.datafile
-	if args.extension == 'csv':
-		c_an.launch_analysis(datafile)
-	elif args.extension == 'xml':
-		x_an.launch_analysis(datafile)
+	
+	try:
+		datafile = args.datafile
+		
+		if datafile == None:
+			raise lg.warning('You must indicate a datafile')
+		else:
+			try:
+				if args.extension == 'csv':
+					c_an.launch_analysis(datafile)
+				elif args.extension == 'xml':
+					x_an.launch_analysis(datafile)
+			except FileNotFoundError as e:
+				lg.critical(':( File not find {} \n'.format(e))  # we must to use format(e) if we want to see error message.
+			finally:
+				lg.debug('########################### Analysis is over    #################################')
+	except Warning as e:
+		lg.warning(e)
+	
+	#import pdb; pdb.set_trace() # pdb = python debuger , pdb.set_trace() for define a breakpoint if we need to check any things / after that we can remove this code line
+	
+	
+	
   
 
 if __name__ == "__main__":
