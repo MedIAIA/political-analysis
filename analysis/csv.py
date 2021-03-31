787c06329e7cd902e267b3da16ec24853ac4ab84
MedIAIA <mohammed.harroud.fr@gmail.com>
2021-04-01 00:21:25 +0200

    update

---
+++ analysis/csv.py
@@ -0,0 +1,30 @@
+import os
+import logging as lg
+
+
+
+
+def launch_analysis(data_file):
+	#print("file_value",__file__)
+	#print(" dir",os.path.dirname(__file__))
+	directory =os.path.dirname(os.path.dirname(__file__)) # return the root dirctory
+	path_to_file = os.path.join(directory, "data", data_file) ## Build of the complet path
+	
+	lg.basicConfig(level=lg.DEBUG) #adding this level
+	
+	with open(path_to_file, 'r') as file:
+		preview = file.readline()
+		lg.debug("\n Yeah! We manged to read the file. here is a preview \n{}".format(preview))
+	
+		
+		
+		
+
+
+def main():
+	print("Main is Run")
+	launch_analysis('current_mps.csv')
+
+
+if __name__ == "__main__":
+	main()
